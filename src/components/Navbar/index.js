eeedda05689ea937219888aaf0d12c9a8a779083
Steve Tong <stevetong53@yahoo.com>
2021-02-17 20:20:04 -0800

    package.json updated

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,27 +3,27 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 function Navbar(props) {
-  const themeMap = {
-    dark: "light",
-    light: "solar",
-    solar: "dark",
-  };
+  // const themeMap = {
+  //   dark: "light",
+  //   light: "solar",
+  //   solar: "dark",
+  // };
 
-  const tmp = Object.keys(themeMap)[0];
+  // const tmp = Object.keys(themeMap)[0];
 
-  const theme =
-    localStorage.getItem("theme") ||
-    (tmp, localStorage.setItem("theme", tmp), tmp);
-  const bodyClass = document.body.classList;
-  bodyClass.add(theme);
+  // const theme =
+  //   localStorage.getItem("theme") ||
+  //   (tmp, localStorage.setItem("theme", tmp), tmp);
+  // const bodyClass = document.body.classList;
+  // bodyClass.add(theme);
 
-  function toggleTheme() {
-    const current = localStorage.getItem("theme");
-    const next = themeMap[current];
+  // function toggleTheme() {
+  //   const current = localStorage.getItem("theme");
+  //   const next = themeMap[current];
 
-    bodyClass.replace(current, next);
-    localStorage.setItem("theme", next);
-  }
+  //   bodyClass.replace(current, next);
+  //   localStorage.setItem("theme", next);
+  // }
 
   return (
     <nav className="navbar">
