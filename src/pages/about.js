01754e394d6cc1b4e45638d603d401d7fa0a7fef
Steve Tong <stevetong53@yahoo.com>
2021-02-19 21:35:19 -0800

    finish with into and about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./about.css";
+import image from "../assests/pfp.jpg";
 
 function About() {
   return (
     <div className="App">
       <div id="header">
-        <span
-          className="font-serif font-semibold tracking-widest text-center text-white-800 pt-1"
-          //   style={"font-size: 35px; color: #08fdd8"}
-        >
+        <span className="font-serif font-semibold tracking-widest text-center text-white-800 pt-1">
           About Me
         </span>
         <br></br>
-        {/* <img src="images/pfp.jpg" width="300" height="300" className="center"> */}
+        <br></br>
+        <img src={image} alt="profile" className="center" />
       </div>
 
       <div id="bioTextWrapper">
@@ -23,14 +22,14 @@ function About() {
           >
             My name is Steve Tong and I am a full-stack web developer, coder,
             and software engineer. I don't have much of experience in real-world
-            coding as I am only 19 years old. I am currently taking the Coding
-            Boot Camp at UCI Continuing Education, where I am furthering my
-            skills and experience. Although I lack any real-world experience in
-            my field, I have experience in various coding languages. I have
-            tackle many projects in HTML, CSS, JavaScript, and C++. I love to
-            work with teammates and thrive off their energy. My future goals and
-            career is to become a software engineer. After gaining experience at
-            a company, I plan to become an entrepreneur with a software or
+            coding as I am in my second year of university. I am currently
+            taking the Coding Boot Camp at UCI Continuing Education, where I am
+            furthering my skills and experience. Although I lack any real-world
+            experience in my field, I have experience in various coding
+            languages and skills. I have tackle many projects in HTML, CSS,
+            JavaScript, and C++. I love to work with teammates and thrive off
+            their energy. My future goals and career is to become a software
+            engineer. I plan to become an entrepreneur with a software or
             program I make. I am an open book that is always ready and willing
             to learn more and new things.
           </p>
